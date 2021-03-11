1eb31464656ed118c803e7599cbfd72c090faa0d
goksucalikoglu <fatmagoksuc@gmail.com>
2021-03-11 19:47:16 +0300

    10 mart

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded=false;
+  constructor(private productService:ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+  }
+}
---
+++ src/app/models/categoryResponseModel.ts
@@ -0,0 +1,7 @@
+import { ResponseModel } from "./responseModel";
+import { Category } from "./category";
+
+export interface CategoryResponseModel extends ResponseModel{
+    data:Category[]
+
+}
